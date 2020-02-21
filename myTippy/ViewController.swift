c4299dc416ca70f775e2e3aeb41dc1d36ff2043b
dinaolmelak <dinaolmelak@gmail.com>
2020-02-20 20:28:39 -0600

    Completed Pre work

--- myTippy/ViewController.swift
+++ myTippy/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculated(_ sender: Any) {
+        // get the data
+        let bill = Double(textField.text!) ?? 0
+        // calculate the tip
+        let tipPercent = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercent[segmentControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // display the tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f",total)
+    }
+    
 }
 
