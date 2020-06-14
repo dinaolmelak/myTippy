082eece813b0fdefd05c6bbaa469878b88973606
= <=>
2020-06-13 22:54:26 -0500

    added animation, separator and first responder

---
+++ myTippy/OptionsViewController.swift
@@ -0,0 +1,79 @@
+//
+//  OptionsViewController.swift
+//  myTippy
+//
+//  Created by Dinaol Melak on 6/13/20.
+//  Copyright © 2020 Dinaol Melak. All rights reserved.
+//
+
+import UIKit
+
+class OptionsViewController: UIViewController {
+
+    
+    var color = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var colorSwitcher: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setColor()
+        // Do any additional setup after loading the view.
+        resetButton.center.y += view.bounds.height
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1) {
+            self.resetButton.center.y -= self.view.bounds.height
+        }
+    }
+    @IBAction func tapSwitch(_ sender: Any) {
+        if colorSwitcher.selectedSegmentIndex == 0{
+            let selectedcolor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            view.backgroundColor = selectedcolor
+            UserDefaults.standard.set(0, forKey: "color")
+        }else if colorSwitcher.selectedSegmentIndex == 1{
+            view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+            UserDefaults.standard.set(1, forKey: "color")
+        }else if colorSwitcher.selectedSegmentIndex == 2{
+            view.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+            UserDefaults.standard.set(2, forKey: "color")
+        }else if colorSwitcher.selectedSegmentIndex == 3{
+            view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            UserDefaults.standard.set(3, forKey: "color")
+        }
+        
+        UserDefaults.standard.set(true, forKey: "switchedColor")
+    }
+    
+    @IBAction func didTapReset(_ sender: Any) {
+        UserDefaults.standard.set(false, forKey: "switchedColor")
+        UserDefaults.standard.set(3, forKey: "color")
+        colorSwitcher.selectedSegmentIndex = 3
+        setColor()
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    func setColor(){
+       colorSwitcher.selectedSegmentIndex = UserDefaults.standard.integer(forKey: "color")
+       if colorSwitcher.selectedSegmentIndex == 0{
+           let selectedcolor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+           view.backgroundColor = selectedcolor
+           
+       }else if colorSwitcher.selectedSegmentIndex == 1{
+           view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+           
+       }else if colorSwitcher.selectedSegmentIndex == 2{
+           view.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+       }else if colorSwitcher.selectedSegmentIndex == 3{
+           view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+       }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+    
+}
--- myTippy/ViewController.swift
+++ myTippy/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var color = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
@@ -18,7 +19,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let check = UserDefaults.standard.bool(forKey: "switchedColor")
+        if check == false{
+            UserDefaults.standard.set(3, forKey: "color")
+        }
+        
+        setColor()
     }
+    override func viewWillAppear(_ animated: Bool) {
+        setColor()
+        textField.becomeFirstResponder()
+    }
+//    override func viewDidAppear(_ animated: Bool) {
+//        setColor()
+//    }
 
     @IBAction func calculated(_ sender: Any) {
         // get the data
@@ -27,10 +41,16 @@ class ViewController: UIViewController {
         let tipPercent = [0.15, 0.18, 0.2]
         let tip = bill * tipPercent[segmentControl.selectedSegmentIndex]
         let total = bill + tip
-        
+        //-----
+        //var fv = 3534234.55
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.maximumFractionDigits = 0;
+        //resultFV.text = formatter.stringFromNumber(fv) // result: $3,534,235 –
+        //-----
         // display the tip
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f",total)
+        tipLabel.text = formatter.string(from: NSNumber(value: tip))
+        totalLabel.text = formatter.string(from: NSNumber(value: total))
     }
     @IBAction func didTapOptions(_ sender: Any) {
         performSegue(withIdentifier: "settingsSegue", sender: self)
@@ -39,5 +59,20 @@ class ViewController: UIViewController {
         
     }
     
+    func setColor(){
+        let storedColor = UserDefaults.standard.integer(forKey: "color")
+        if storedColor == 0{
+            view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            
+        }else if storedColor == 1{
+            view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+            
+        }else if storedColor == 2{
+            view.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+            
+        }else if storedColor == 3{
+            view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
 }
-
